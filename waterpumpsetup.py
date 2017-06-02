414940d1bd50262690b49ad00849fd6d8df9abeb
andrew <andyhasv8@gmail.com>
2017-06-02 23:30:44 +1000

    update to analog moisture sensor

--- waterpumpsetup.py
+++ waterpumpsetup.py
@@ -1,9 +1,13 @@
 import RPi.GPIO as GPIO
 import time
+import Adafruit_ADS1x15
 
 moisture_channel = 17
 pump_channel = 27
 pump_duration = 60
+moisty_val = 20000
+
+adc = Adafruit_ADS1x15.ADS1115()
 
 def runPump():
     print("running pump")
@@ -17,9 +21,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(moisture_channel, GPIO.IN)
 GPIO.setup(pump_channel, GPIO.OUT)
 
-result = GPIO.input(moisture_channel)
-
-if result is 1:
+result = adc.read_adc(0, gain=1)
+print(result)
+if result > moisty_val:
     print("low water")
     runPump()
 else:
